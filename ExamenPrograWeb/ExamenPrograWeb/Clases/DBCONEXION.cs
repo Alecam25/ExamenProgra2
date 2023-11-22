395731d6a69a05334a33049f40d23d94b63ee686
Alecam25 <Alessandro.cam.2005@gmail.com>
2023-11-22 02:31:32 -0600

    Entrega Examen

---
+++ ExamenPrograWeb/ExamenPrograWeb/Clases/DBCONEXION.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ExamenPrograWeb.Clases
+{
+    public class DBCONEXION
+    {
+        public static SqlConnection obtenerConexion()
+        {
+            string s = System.Configuration.ConfigurationManager.ConnectionStrings["CONEXION"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(s);
+            conexion.Open();
+            return conexion;
+        }
+
+
+    }
+}
---
+++ ExamenPrograWeb/ExamenPrograWeb/Inicio.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="ExamenPrograWeb.Inicio1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="container text-center">
+
+       <h1> Pagina de Inicio </h1> 
+
+   </div>
+</asp:Content>
